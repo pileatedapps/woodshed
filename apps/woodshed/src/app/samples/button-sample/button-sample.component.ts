648c9d674142b3a664657260d9bc1dc4e8356a95
Joe White <jwhite1802@gmail.com>
2024-11-09 17:03:39 -0500

    Updates styles for dark theme

--- apps/woodshed/src/app/samples/button-sample/button-sample.component.ts
+++ apps/woodshed/src/app/samples/button-sample/button-sample.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatAnchor, MatButton, MatFabButton, MatIconButton, MatMiniFabButton } from '@angular/material/button';
 import { MatDivider } from '@angular/material/divider';
 import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-button-sample',
   standalone: true,
-  imports: [CommonModule, MatAnchor, MatButton, MatDivider, MatFabButton, MatIcon, MatIconButton, MatMiniFabButton],
+  imports: [CommonModule, MatAnchor, MatButton, MatDivider, MatFabButton, MatIcon, MatIconButton, MatMiniFabButton, RouterLink],
   templateUrl: './button-sample.component.html',
   styleUrl: './button-sample.component.scss',
 })
