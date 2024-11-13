5ca8bc1bb37a0b0e0dc8bf707e69d3539f281953
Joe White <jwhite1802@gmail.com>
2024-11-12 21:29:45 -0500

    increases coverage

--- apps/woodshed/src/app/samples/_samples/samples.component.spec.ts
+++ apps/woodshed/src/app/samples/_samples/samples.component.spec.ts
@@ -1,19 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SamplesComponent } from './samples.component';
-import { provideRouter } from '@angular/router';
+import { ActivatedRoute, provideRouter, Router } from '@angular/router';
+import { appRoutes } from '../../app.routes';
 
 describe('SamplesComponent', () => {
   let component: SamplesComponent;
   let fixture: ComponentFixture<SamplesComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SamplesComponent],
       providers: [
-        provideRouter([])
+        provideRouter(appRoutes),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              title: 'Parent Title',
+              children: [
+                {
+                  routeConfig: {
+                    path: '/Child01'
+                  },
+                  title: 'Child 01'
+                },
+                {
+                  routeConfig: {
+                    path: ':childid'
+                  },
+                  title: 'Child By Id'
+                }
+              ]
+            },
+          }
+        }
       ]
     }).compileComponents();
 
+    router = TestBed.inject(Router);
     fixture = TestBed.createComponent(SamplesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -22,4 +47,10 @@ describe('SamplesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should only have links for snapshots that have titles but are not ids', () => {
+    expect(component.links.length).toEqual(1);
+  })
+
+
 });
