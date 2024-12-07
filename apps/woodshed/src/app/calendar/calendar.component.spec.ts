bc2ecd4045676be42eb4d67901c8c31fb3f78fff
Joe White <jwhite1802@gmail.com>
2024-12-07 15:37:43 -0500

    corrects test

--- apps/woodshed/src/app/calendar/calendar.component.spec.ts
+++ apps/woodshed/src/app/calendar/calendar.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CalendarComponent } from './calendar.component';
+import { provideRouter } from '@angular/router';
 
 describe('CalendarComponent', () => {
   let component: CalendarComponent;
@@ -8,6 +9,9 @@ describe('CalendarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CalendarComponent],
+      providers: [
+        provideRouter([])
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(CalendarComponent);
