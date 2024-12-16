2d343a1a43716e77953025e2ffe668fb422d2cd2
jwhite1802 <jwhite1802@users.noreply.github.com>
2024-12-15 20:59:05 -0500

    refactors to remove unnecassry code dupe (#12)

--- libs/calendar/src/lib/components/calendar/pily8-calendar-toolbar/pily8-calendar-toolbar.component.ts
+++ libs/calendar/src/lib/components/calendar/pily8-calendar-toolbar/pily8-calendar-toolbar.component.ts
@@ -8,13 +8,15 @@ import { MatSuffix } from '@angular/material/form-field';
 import { ActivatedRoute, ParamMap, Router, RouterOutlet } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Pily8CalendarService } from '../../../service/pily8-calendar.service';
+import { MatOption, MatSelect } from '@angular/material/select';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
 
-export type CalType = 'month' | 'week' | 'agenda';
+export type CalType = 'Day' | 'Week' | 'Month' | 'Agenda';
 
 @Component({
   selector: 'pily8-cal-calendar-toolbar',
   standalone: true,
-  imports: [CommonModule, MatButton, MatIcon, MatToolbar, MatTooltip, MatIconButton, MatSuffix, RouterOutlet],
+  imports: [CommonModule, MatButton, MatIcon, MatToolbar, MatTooltip, MatIconButton, MatSuffix, RouterOutlet, MatSelect, MatOption, MatMenuTrigger, MatMenu, MatMenuItem],
   templateUrl: './pily8-calendar-toolbar.component.html',
   styleUrl: './pily8-calendar-toolbar.component.scss',
 })
@@ -22,13 +24,15 @@ export class Pily8CalendarToolbarComponent implements OnInit, OnDestroy {
   private readonly route: ActivatedRoute = inject(ActivatedRoute);
   private readonly router: Router = inject(Router);
   private readonly calendarService: Pily8CalendarService = inject(Pily8CalendarService);
+  calType: CalType = 'Month';
   private routeParam$!: Subscription;
   selectedDate: Date = new Date(new Date().setHours(0,0,0,0));
 
   ngOnInit(): void {
+    this.calType = this.route.snapshot.data['calType'];
     this.routeParam$ = this.route.paramMap
       .subscribe((paramMap: ParamMap) => {
-        this.selectedDate = this.calendarService.setDateFromRoute(paramMap)
+        this.selectedDate = this.calendarService.setDateFromRoute(paramMap);
       })
   }
 
@@ -36,26 +40,33 @@ export class Pily8CalendarToolbarComponent implements OnInit, OnDestroy {
     this.routeParam$?.unsubscribe();
   }
 
-  adjustMonth(date: Date, increment: number): Date {
-    const newDate = new Date(date);
+  adjustDate(newDate: Date, increment: 1 | -1 | 0): Date {
     newDate.setHours(0,0,0,0);
-    newDate.setMonth(newDate.getMonth() + increment);
-    newDate.setDate(1);
+    switch (this.calType) {
+      case 'Agenda':
+        break;
+      case 'Day':
+        newDate.setDate(newDate.getDate() + increment);
+        break;
+      case 'Week':
+        newDate.setDate(newDate.getDate() + (7 * increment));
+        break;
+      case 'Month':
+        newDate.setMonth(newDate.getMonth() + increment);
+        break;
+    }
     return newDate;
   }
 
-  adjustYear(date: Date, increment: number): Date {
-    const newDate = new Date(date);
-    newDate.setHours(0,0,0,0);
-    newDate.setFullYear(newDate.getFullYear() + increment);
-    newDate.setDate(1);
-    return newDate;
+  async changeCalendarType(calType: CalType) {
+    this.calType = calType;
+    await this.navigateToDate(this.selectedDate);
   }
 
-  async navigateToDate(newDate: Date = new Date(), type: CalType = 'month') {
+  async navigateToDate(newDate: Date = new Date()) {
     await this.router.navigate([
       '/calendar',
-      type,
+      this.calType,
       newDate.getFullYear(),
       newDate.getMonth() + 1,
       newDate.getDate()
